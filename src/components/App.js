70d630ef109b6560a63d19da3041e7758a60de67
christelle01 <christellecarmela@gmail.com>
2022-08-09 17:26:25 +0000

    event and state

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import Banner from './Banner'
+import logo from '../assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './ShoppingList'
-
+import QuestionForm  from './QuestionForm'
 
 function App() {
 	return (
 		<div>
-			<Banner />
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
 			{/* <Cart /> */}
-      <ShoppingList />
+			<ShoppingList />
+			<QuestionForm />
 		</div>
 	)
 }
